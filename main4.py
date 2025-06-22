643d8b831ea420b6bb1e085073d856084a351dcf
pikitosdoritos <fastovetsn@gmail.com>
2025-06-22 23:18:40 +0300

    import module pprint

--- main4.py
+++ main4.py
@@ -1,3 +1,5 @@
+import pprint
+
 message = 'It was a bright cold day in April, and the clocks were striking thirteen'
 count = {}
 
@@ -5,4 +7,4 @@ for character in message:
     count.setdefault(character, 0)
     count[character] = count[character] + 1
 
-print(count)
+pprint.pprint(count)
